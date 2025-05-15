37476b3d5d3bb224356cc0f88a468d3063b241e2
Adam Barsness <ajbarsness@gmail.com>
2025-05-15 07:08:18 +0100

    Tested the Initial Commit

--- main.py
+++ main.py
@@ -2,17 +2,22 @@ import os
 
 # Import dependancies
 from PIL import Image
-from inky.inky import eeprom
-from inky.inky import phat
+from inky import eeprom
+from inky import phat
 
 # Import secrets
-from .secrets import Secrets
+from secrets import Secrets
+
+# Debug boolean
+DEBUG = True
 
 # Connect to the Inky Display
 _eeprom = eeprom.read_eeprom()
 if _eeprom is None:
     raise RuntimeError("No EEPROM detected! You must manually initialise your Inky board.")
-elif _eeprom is not 11:
+elif _eeprom.display_variant != 11:
+    if DEBUG:
+        print("Found eeprom id " + _eeprom.display_variant)
     raise RuntimeError("Display is not the expected variant")
 
 display = phat.InkyPHAT_SSD1608("red")
@@ -23,6 +28,6 @@ try:
 except NotImplementedError:
     pass
 
-img = Image.open(os.path.join("/home/", Secrets.username, "inky/examples/phat/resources/InkypHAT-250x122.png"))
+img = Image.open(os.path.join("/home/", Secrets.username, "repos/inky/examples/phat/resources/InkypHAT-250x122.png"))
 display.set_image(img)
 display.show()
